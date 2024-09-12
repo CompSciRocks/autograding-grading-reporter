f231e324ef667987db697101f465e64f56b52812
Ryan Nutt <ryan@nutt.com>
2024-09-12 08:50:19 -0500

    ready for first test

---
+++ src/send-feedback.js
@@ -0,0 +1,136 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { getTestScore, getMaxScoreForTest } = require('./helpers/test-helpers')
+
+exports.SendFeedback = async function SendFeedback(runnerResults) {
+
+    let shouldSend = core.getInput("send_feedback");
+
+    // Has to be either always, never, or when_available
+    if (shouldSend !== "always" && shouldSend !== "never" && shouldSend !== "when_available") {
+        core.setFailed("send_feedback must be either 'always', 'never', or 'when_available'");
+        return;
+    }
+
+    if (shouldSend === "never") {
+        // Skipping feedback
+        return;
+    }
+
+    let markdownList = [];
+
+    runnerResults.forEach(({ runner, results }) => {
+        if (results.markdown && results.markdown.length > 0) {
+            markdownList.push(results.markdown);
+        }
+    });
+
+    if (shouldSend === 'when_available' && markdownList.length === 0) {
+        // No feedback from the runners, settings say to skip
+        return;
+    }
+
+    let maxScore = 0;
+    let score = 0;
+
+    runnerResults.forEach(({ results }) => {
+        maxScore += results.max_score;
+        results.tests.forEach((test) => {
+            score += test.score;
+        });
+    });
+
+    let markdownText = '';
+
+    markdownText = `# Feedback from autograding\n\n`;
+    markdownText += 'Date: ' + new Date().toLocaleString() + '\n\n';
+
+    if (maxScore > 0) {
+        // No button if no max score
+        if (maxScore == score) {
+            // 100%, green badge
+            markdownText += '![](https://img.shields.io/badge/grading-' + score + '/' + maxScore + '-green)\n\n';
+        } else if (score == 0) {
+            // 0%, red badge
+            markdownText += '![](https://img.shields.io/badge/grading-' + score + '/' + maxScore + '-red)\n\n';
+        } else {
+            // Partial credit, yellow badge
+            markdownText += '![](https://img.shields.io/badge/grading-' + score + '/' + maxScore + '-yellow)\n\n';
+        }
+    }
+
+    if (markdownList.length > 0) {
+        markdownText += markdownList.join('\n\n');
+    }
+
+    const token = process.env.GITHUB_TOKEN || core.getInput("token");
+    if (!token || token === "") {
+        core.setFailed("No token provided");
+        return;
+    }
+
+    const octokit = github.getOctokit(token);
+    if (!octokit) {
+        core.setFailed("Failed to create octokit");
+        return;
+    }
+
+    const nwo = process.env.GITHUB_REPOSITORY || "/";
+    const [owner, repo] = nwo.split("/");
+    if (!owner) {
+        core.setFailed("Failed to get owner");
+        return;
+    }
+
+    if (!repo) {
+        core.setFailed("Failed to get repo");
+        return;
+    }
+
+    // Get all PRs and find the first with title "Feedback"
+    const { data: pullRequests } = await octokit.rest.pulls.list({
+        owner,
+        repo
+    });
+
+    let feedbackPR = null;
+
+    for (let i = 0; i < pullRequests.length; i++) {
+        const pr = pullRequests[i];
+        if (pr.title === "Feedback") {
+            feedbackPR = pr;
+            break;
+        }
+    }
+
+    if (!feedbackPR) {
+        // Create the PR and get the id
+        const { data: newPR } = await octokit.rest.pulls.create({
+            owner,
+            repo,
+            title: 'Feedback',
+            head: 'feedback',
+            base: 'main',
+            body: 'This pull request is a place for you and your teacher to discuss your code. **Do not close or merge this pull request.**',
+        });
+
+        feedbackPR = newPR;
+    }
+
+    // Get the id
+    const feedbackPRNumber = feedbackPR.number;
+
+    try {
+        await octokit.rest.issues.createComment({
+            owner,
+            repo,
+            issue_number: feedbackPRNumber,
+            body: markdownText,
+            title: "Autograding Feedback: " + new Date().toLocaleString()
+        });
+    } catch (error) {
+        core.setFailed("Failed to create comment: " + error.message);
+    }
+
+
+}
